cc2333833beb17846652472ac43da5ae67080344
Patrick Hardy <HardyP@aetna.com>
2017-10-12 12:37:43 -0600

    create days of week selector

--- src/components/DaySelector/DaySelector.js
+++ src/components/DaySelector/DaySelector.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react'
 import './DaySelector.css'
 
 class DaySelector extends Component {
+  state = {
+    daysOfWeek: []
+  }
+
+  createWeekFromToday = () => {
+    const today = new Date().getDay()
+    const daysOfWeek = ['Sunday', 'Monday', 'Teusday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const removedDays = daysOfWeek.splice(0, today)
+    return daysOfWeek.concat(removedDays, daysOfWeek[0])
+  }
+
+  componentDidMount () {
+    this.setState({ daysOfWeek: this.createWeekFromToday() })
+  }
+
   render () {
+    const { daysOfWeek } = this.state
     return (
-      <p>hi hi hi</p>
+      <select>
+        {daysOfWeek.map((day, index) =>
+          <option key={`${day}-${index}`} value={index}>{day}</option>
+        )}
+      </select>
     )
   }
 }
