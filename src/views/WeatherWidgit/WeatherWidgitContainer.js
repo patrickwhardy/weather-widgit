00b404fef21b2b82c6a464fe1aedbb25d5fcd610
Patrick Hardy <HardyP@aetna.com>
2017-10-13 14:18:25 -0600

    weatherWidgitContainer refactor

--- src/views/WeatherWidgit/WeatherWidgitContainer.js
+++ src/views/WeatherWidgit/WeatherWidgitContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import * as DarkSkyAPI from '../../utils/DarkSkyAPI'
 import WeatherWidgit from '../../components/WeatherWidgit/WeatherWidgit'
 import { connect } from 'react-redux'
-import { changeSelectedForecast } from '../../actions'
 import './WeatherWidgitContainer.css'
 
 class WeatherWidgitContainer extends Component {
@@ -20,18 +19,15 @@ class WeatherWidgitContainer extends Component {
   }
 
   setWeatherData = (index = this.state.forecastIndex) => {
-    if (this.state.forecastData[index]) {
-      const selectedForecast = this.state.forecastData[index]
-      return {
+    const selectedForecast = this.state.forecastData[index]
+    return selectedForecast ?
+      {
         high: parseInt(selectedForecast.temperatureMax, 10),
         low: parseInt(selectedForecast.temperatureMin, 10),
         wind: parseInt(selectedForecast.windSpeed, 10),
         precip: parseInt(selectedForecast.precipProbability * 100, 10),
         icon: selectedForecast.icon,
-      }
-    } else {
-      return {}
-    }
+      } : {}
   }
 
   render () {
@@ -54,6 +50,5 @@ const mapStateToProps = (state, props) => {
 }
 
 export default connect(
-  mapStateToProps,
-  { changeSelectedForecast }
+  mapStateToProps
 )(WeatherWidgitContainer)
